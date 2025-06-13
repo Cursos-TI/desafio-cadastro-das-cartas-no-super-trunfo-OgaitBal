69ea5cf8cc7a67e4385b2a4d0fbc640c43a62f6e
OgaitBal <tiagoba_99@hotmail.com>
2025-06-13 22:21:36 +0000

    Desafio Cadastro Cartas.

---
+++ Cartas.c
@@ -0,0 +1,41 @@
+int main() {
+    int carta = 1;
+    char estado[20] = "Bahia";
+    char codigo[5] = "A01";
+    char cidade[20] = "Salvador";
+    float populacao = 856.458;
+    float area = 1635.22;
+    float pib = 425;
+    int turistico = 95;
+    
+    int carta2 = 2;
+    char estado2[20] = "Sergipe";
+    char codigo2[5] = "A02";
+    char cidade2[20] = "Aracaju";
+    float populacao2 = 1452.25;
+    float area2 = 835.22;
+    float pib2 = 652;
+    int turistico2 = 78;
+
+
+    printf("Carta: %d\n", carta);
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %.2f\n", populacao);
+    printf("Área: %.2fKm²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", turistico);
+    printf("\n \n");
+    printf("Carta: %d\n", carta2);
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %.2f\n", populacao2);
+    printf("Área: %.2fKm²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", turistico2);
+
+
+    
+}
